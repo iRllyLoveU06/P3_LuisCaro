bded70c75f84b43fc1ad85998515ddbfdccd9485
iRllyLoveU06 <Angel.caro@Udea.edu.Co>
2025-11-08 19:49:30 -0500

    Primeros arvhivos y las librerias

---
+++ clases.py
@@ -0,0 +1,10 @@
+# ---  Importación de Librerías ---
+import os  # Para interactuar con el sistema operativo (leer carpetas)
+import pydicom  # Para leer los archivos DICOM
+import numpy as np  # Para el manejo de arreglos 
+import cv2  # OpenCV para procesamiento de imagen
+import matplotlib.pyplot as plt  # Para mostrar las imágenes
+import nibabel as nib  # Para crear y guardar archivos NIFTI
+from datetime import datetime  # Para calcular la diferencia de tiempo]
+
+
